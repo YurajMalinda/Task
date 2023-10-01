7ed0aef69ae0312b523867af1ac32c35492c69d0
YurajMalinda <yurajmalinda123.com>
2023-10-01 16:09:05 +0530

    Content updated.

--- assests/Task.js
+++ assests/Task.js
@@ -1,25 +1,10 @@
-// document.querySelector(".h1Class").addEventListener("click",function(){
-//     document.querySelector(".h1Class").remove()
-// });
+let btn=document.querySelector("#btn");
+let ol=document.querySelector("#list");
 
-// document.querySelector("button").addEventListener("click",function(){
-//     document.querySelector(".h1Class").innerText=document.querySelector('input').value;
-// });
-
-document.querySelector("input").addEventListener("keyup",function(){
-    document.querySelector("h1").innerText=document.querySelector("input").value
+btn.addEventListener("click",function (){
+    let typInp=document.querySelector("#txt").value;
+    let li=document.createElement("li");
+    li.setAttribute("class","list-group-item");
+    li.textContent=typInp;
+    ol.append(li);
 });
-
-document.querySelector("#btn1").addEventListener("click",function(){
-    document.querySelector("h1").style.color="red";
-});
-
-document.querySelector("#btn2").addEventListener('click',function(){
-    var typeValue=document.querySelector("#url").value;
-    var divContainer=document.querySelector("#img");
-
-    divContainer.style.background=`url(${typeValue})`;
-    divContainer.style.backgroundPosition='center';
-    divContainer.style.backgroundSize='contain';
-    divContainer.style.backgroundRepeat='no-repeat';
-})
