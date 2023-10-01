9d124a171cbe5504348c623e820674eca7e37d7f
YurajMalinda <yurajmalinda123.com>
2023-10-01 11:48:13 +0530

    Content updated.

--- assests/Task.js
+++ assests/Task.js
@@ -1,3 +1,7 @@
-document.querySelector(".h1Class").addEventListener("click",function(){
-    document.querySelector(".h1Class").remove()
+// document.querySelector(".h1Class").addEventListener("click",function(){
+//     document.querySelector(".h1Class").remove()
+// });
+
+document.querySelector("button").addEventListener("click",function(){
+    document.querySelector(".h1Class").innerText=document.querySelector('input').value;
 });
