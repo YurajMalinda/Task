dfdf2f5fb9dd496793b4d559681f120503292bf5
YurajMalinda <yurajmalinda123.com>
2023-10-01 12:17:05 +0530

    listen to typing and change color to red when clicked.

--- assests/Task.js
+++ assests/Task.js
@@ -2,6 +2,14 @@
 //     document.querySelector(".h1Class").remove()
 // });
 
-document.querySelector("button").addEventListener("click",function(){
-    document.querySelector(".h1Class").innerText=document.querySelector('input').value;
+// document.querySelector("button").addEventListener("click",function(){
+//     document.querySelector(".h1Class").innerText=document.querySelector('input').value;
+// });
+
+document.querySelector("input").addEventListener("keyup",function(){
+    document.querySelector("h1").innerText=document.querySelector("input").value
+});
+
+document.querySelector("#btn1").addEventListener("click",function(){
+    document.querySelector("h1").style.color="red";
 });
